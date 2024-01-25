fb49802637a57ef19fb405265c957b5241ac2b7e
Wooboy <github@wooboy.me>
2024-01-25 11:47:41 +0800

    refactoring

--- Veeam/Export-VeeamToVMDK.ps1
+++ Veeam/Export-VeeamToVMDK.ps1
@@ -1,3 +1,10 @@
+$ErrorActionPreference = 'Continue'
+
+# ==== SCRIPT SETTINGS ====
+$VBKSourceRoot = 'F:\Backup_Repo'
+$DestPath = "E:\DR-VMDK"
+$vms = @('VM1','VM2')
+# ==== SCRIPT SETTINGS END ====
 
 function Out-LogFile {
     param (
@@ -97,7 +104,10 @@ function Export-VeeamToVMDK {
         # 匯出成功
         $endtime = Get-date
         $timespan = $endtime - $starttime
-        Out-LogFile -Level Info -Content "Export OK【${VMName}@$($RestorePoint.CreationTime.tostring('s'))】`n                         Spent $(($timespan.Hours).ToString('00')):$(($timespan.Minutes).ToString('00')):$(($timespan.Seconds).ToString('00'))" -LogFilePath "${env:Temp}\Restore_${VMName}.log"
+        $logmsg = "Export OK【${VMName}@$($RestorePoint.CreationTime.tostring('s'))】"
+        $logmsg += "`n"
+        $logmsg += "                              Spent $(($timespan.Hours).ToString('00')):$(($timespan.Minutes).ToString('00')):$(($timespan.Seconds).ToString('00'))"
+        Out-LogFile -Level Info -Content $logmsg -LogFilePath "${env:Temp}\Restore_${VMName}.log"
 
         # 如果正式目錄已存在舊檔，先刪除
         if (Test-Path "${OutPath}\${VMName}") {
@@ -107,7 +117,7 @@ function Export-VeeamToVMDK {
 
         # 重命名為正式目錄
         Rename-Item "${OutPath}\${VMName}-Temp" "${OutPath}\${VMName}" -Force
-        if(Test-Path "${OutPath}\${VMName}"){
+        if (Test-Path "${OutPath}\${VMName}") {
             Out-LogFile -Level Info -Content "Move to ${OutPath}\${VMName} OK" -LogFilePath "${env:Temp}\Restore_${VMName}.log"
         }
 
@@ -125,4 +135,90 @@ function Export-VeeamToVMDK {
         }
         return $false
     }
+}
+
+Out-LogFile -Level Info -Content "Start Export VMDK" -LogFilePath "${env:Temp}\Restore_VMDK.log"
+
+# 開啟連線
+$RestoreServer = $null
+# 先檢查是否已連線
+try {
+    $RestoreServer = (Get-VBRServer -ErrorAction SilentlyContinue | Where-Object type -eq 'Local')
+}
+catch {
+    Connect-VBRServer -Server localhost
+    $RestoreServer = (Get-VBRServer -ErrorAction SilentlyContinue .\.affinityilentlyContinue | Where-Object type -eq 'Local')
+}
+
+# 如果沒有建立連線，回覆錯誤並結束
+if ($null -eq $RestoreServer) {
+    Write-Host "Connection error"
+    Out-LogFile -Level Error -Content "Connection error" -LogFilePath "${env:Temp}\Restore_VMDK.log"
+}
+else {
+    
+    foreach ($VMName in $vms) {
+        Write-Host "Process ${VMName}"
+        Out-LogFile -Level Info -Content "Start Export 【${VMName}】" -LogFilePath "${env:Temp}\Restore_${VMName}.log"
+        
+        # 檢查要匯出的主機是否在本機備份清單中
+        if ((Get-VBRJobObject -Job $(Get-VBRJob)).Name -contains ${VMName}) {
+            # 有就直接匯出
+            $result = Export-VeeamToVMDK -VMName ${VMName} -OutPath ${DestPath}
+        }
+        else {
+            # 如果沒有，跑匯入備份檔並匯出的流程
+
+            # 找到 VBK 檔
+            $VBKSource = Get-ChildItem -Recurse -File "$VBKSourceRoot" | Where-Object { $_.Extension -eq '.vbk' -and $_.Name -like "$($VMName).*" } | Sort-Object LastWriteTime -Descending | Select-Object Name, Fullname, LastWriteTime -First 1
+
+            # 如果有找到再繼續
+            if (-not [string]::IsNullOrEmpty($VBKSource.Name)) {
+                # 匯入 VBK
+                try {
+                    Out-LogFile -Level Info -Content "import $($VBKSource.Name)" -LogFilePath "${env:Temp}\Restore_${VMName}.log"
+                    Import-VBRBackup -Server $RestoreServer -FileName "$($VBKSource.Fullname)"
+                }
+                catch {
+                    Out-LogFile -Level Error -Content "import ${VMName} fail" -LogFilePath "${env:Temp}\Restore_${VMName}.log"
+                }
+
+                # 開始匯出
+                $result = Export-VeeamToVMDK -VMName ${VMName} -OutPath ${DestPath}
+                if ($result) {
+                    # 成功匯出後移除匯入檔
+                    #$imported = Get-VBRBackup -Name '*imported'
+                    #if ($imported.count -gt 0) {
+                    #if ((Get-VBRRestorePoint -Backup $imported -Name $VMName).Count -gt 0) {
+                    #"$(Get-Date -Format s) !!!! [WRAN] 【${VMName}】 imported" | Out-File -append "${DestPath}\Restore_${VMName}.log"
+                    #}
+                    #Remove-VBRBackup -Backup $imported -Confirm
+                    #}
+                }
+                else {
+                
+                }
+            }
+            else {
+                Out-LogFile -Level Error -Content "${VMName} Not found" -LogFilePath "${env:Temp}\Restore_${VMName}.log"
+            }
+        }
+
+        if (Test-Path "${env:Temp}\Restore_${VMName}.log") {
+            New-Item -ItemType Directory "${DestPath}\XXX-Log" -Force -ErrorAction SilentlyContinue | Out-Null
+            Move-Item "${env:Temp}\Restore_${VMName}.log" "${DestPath}\XXX-Log" -Force
+            Rename-Item "${DestPath}\XXX-Log\Restore_${VMName}.log" "Restore_${VMName}-$(Get-Date -Format 'yyyyMMddHHmmss').log" -Force
+        }
+    }
+
+    # 關閉連線
+    Disconnect-VBRServer
+}
+
+Out-LogFile -Level Info -Content "  End Export VMDK" -LogFilePath "${env:Temp}\Restore_VMDK.log"
+
+if (Test-Path "${env:Temp}\Restore_VMDK.log") {
+    New-Item -ItemType Directory "${DestPath}\XXX-Log" -Force -ErrorAction SilentlyContinue | Out-Null
+    Move-Item "${env:Temp}\Restore_VMDK.log" "${DestPath}\XXX-Log" -Force
+    Rename-Item "${DestPath}\XXX-Log\Restore_VMDK.log" "Restore_VMDK-$(Get-Date -Format 'yyyyMMddHHmmss').log" -Force
 }
